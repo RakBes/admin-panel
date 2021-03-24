3dfcc50c5839918e615e31157211423c1e82fe3e
LavKar <VVLRAKOV@SBERBANK.RU>
2021-03-24 21:27:33 +0400

    Lesson-1

---
+++ script.js
@@ -0,0 +1,17 @@
+// alert('Hello, World!');
+
+// console.log('Hi!');
+// console.warn('Warn');
+// console.error('Error');
+
+// let age = prompt('Сколько тебе лет?', 100);
+// alert(`Тебе ${age} лет!`);
+
+// let isBose = confirm("Ты здесь главный?");
+// alert(isBose); 
+
+let x;
+x=6;
+const y=12;
+var z=1;
+console.log(x);
